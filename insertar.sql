3ab572b75d9eff5dc26c976376cf981bcf3b47e1
Carlos Poveda <carlpokun@gmail.com>
2017-04-08 10:46:27 -0400

    Insercion de Estudiantes
    Pa ve creo que es esto, insercion de estudiantes

---
+++ insertar.sql
@@ -0,0 +1,18 @@
+INSERT INTO mtn.Estudiante
+VALUES('243275454','Los Rompe Codigos','123456789','Tchaikovsky Perez','Computacion',2010),
+	  ('568921454','Los Rompe Codigos','123456789','Stravinsky Sanchez','Computacion',2012),
+	  ('879554574','Los Rompe Codigos','123456789','Anna Mozart','Computacion',2011),
+	  ('243333454','La Faccion EseCuEle','987654321','Donald Chamberlin','Computacion',2002),
+	  ('243275455','La Faccion EseCuEle','987654321','Edgar Frank Codd','Computacion',2006),
+	  ('243275584','La Faccion EseCuEle','987654321','Helena Tesla','Computacion',2010),
+	  ('243587454','La Legion Vtelca','456789357','Bill Gates','Computacion',2008),
+	  ('343275454','La Legion Vtelca','456789357','Emma Watson','Computacion',2009),
+	  ('563225454','La Legion Vtelca','456789357','Zoraya Montenegro','Computacion',2015),
+	  ('25475454','Escuadron Suicida','321987654','Mister Sayaman','Computacion',2007),
+	  ('698275454','Escuadron Suicida','321987654','Krilin Feinaiquel','Computacion',2007),
+	  ('221457545','Escuadron Suicida','321987654','Son Goku','Computacion',2007),
+	  ('235478965','El Cartel de los Soles','555555555','Jhonny Bravo','Computacion',2008),
+	  ('227895465','El Cartel de los Soles','555555555','Jackie Chan','Computacion',2007),
+	  ('343275459','El Cartel de los Soles','555555555','Astronema Hernandez','Computacion',2006);
+
+
