62e556fddd0e64f67bdf55bf688c0140cdd47f66
Carlos Poveda <carlpokun@gmail.com>
2017-04-10 22:48:43 -0400

    Unos cuantos coachs con area de experticia nula
    Los deje en nula el area de experticia porque creo que eso es solo para
    los tecnicos, pero no estoy seguro jejep

--- insertar.sql
+++ insertar.sql
@@ -5,7 +5,7 @@ VALUES('243275454','Los Rompe Codigos','123456789','Tchaikovsky Perez','Computac
 	  ('243333454','La Faccion EseCuEle','987654321','Donald Chamberlin','Computacion',1),
 	  ('243275455','La Faccion EseCuEle','987654321','Edgar Frank Codd','Computacion',2),
 	  ('243275584','La Faccion EseCuEle','987654321','Helena Tesla','Computacion',3),
-	  ('243587454','La Legion Vtelca','456789357','Bill Gates','Computacion',5),
+	  ('243587454','La Legion Vtelca','456789357','Vill Gates','Computacion',5),
 	  ('343275454','La Legion Vtelca','456789357','Emma Watson','Computacion',4),
 	  ('563225454','La Legion Vtelca','456789357','Zoraya Montenegro','Computacion',2),
 	  ('25475454','Escuadron Suicida','321987654','Mister Sayaman','Computacion',3),
@@ -20,5 +20,9 @@ VALUES('Los Rompe Codigos','Universidad de Carabobo','04-03-2008','El Programato
 	  ('La Legion Vtelca','Universidad de Carabobo','09-05-2009','El Maraton Intergalactico de Programacion 23','Clases en el Espacio con Yoda','De otro mundo'),
 	  ('Escuadron Suicida','Universidad de Carabobo','09-05-2009','El Maraton Intergalactico de Programacion 23','Clases en el Espacio con Yoda','De otra Galaxia'),
 	  ('El Cartel de los Soles','Universidad de Carabobo','19-08-2003','Maraton de Programacion Shaolin','Clases de C con Miyagi','Aceptable');
-
-
+INSERT INTO mtn.Profesor
+VALUES('44445558','Maestro Yoda','Escuadron Suicida','Coach','','Programacion Intergalactica','0800-STAR'),
+	  ('33325488','Bill Gates','La Faccion EseCuEle','Coach','','Algoritmos','0800-MICROSOFT'),
+	  ('87456214','Maestro Miyagi','El Cartel de los Soles','Coach','','Programacion C','0800-MIYAGI'),
+	  ('85858745','Gordom Ramsay','Los Rompe Codigos','Coach','','Cocina','0800-PIZZA'),
+	  ('25412568','Avril Lavigne','La Faccion EseCuEle','Coach','','Programacion Paralela','0500-ROCKSTAR');
