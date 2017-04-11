7a7083e36b921d3563c156b4d560e2c1b418e788
Carlos Poveda <carlpokun@gmail.com>
2017-04-10 21:58:05 -0400

    Insercion de unos equipitos + los datos esos raros de las tablas

--- insertar.sql
+++ insertar.sql
@@ -1,18 +1,24 @@
 INSERT INTO mtn.Estudiante
-VALUES('243275454','Los Rompe Codigos','123456789','Tchaikovsky Perez','Computacion',2010),
-	  ('568921454','Los Rompe Codigos','123456789','Stravinsky Sanchez','Computacion',2012),
-	  ('879554574','Los Rompe Codigos','123456789','Anna Mozart','Computacion',2011),
-	  ('243333454','La Faccion EseCuEle','987654321','Donald Chamberlin','Computacion',2002),
-	  ('243275455','La Faccion EseCuEle','987654321','Edgar Frank Codd','Computacion',2006),
-	  ('243275584','La Faccion EseCuEle','987654321','Helena Tesla','Computacion',2010),
-	  ('243587454','La Legion Vtelca','456789357','Bill Gates','Computacion',2008),
-	  ('343275454','La Legion Vtelca','456789357','Emma Watson','Computacion',2009),
-	  ('563225454','La Legion Vtelca','456789357','Zoraya Montenegro','Computacion',2015),
-	  ('25475454','Escuadron Suicida','321987654','Mister Sayaman','Computacion',2007),
-	  ('698275454','Escuadron Suicida','321987654','Krilin Feinaiquel','Computacion',2007),
-	  ('221457545','Escuadron Suicida','321987654','Son Goku','Computacion',2007),
-	  ('235478965','El Cartel de los Soles','555555555','Jhonny Bravo','Computacion',2008),
-	  ('227895465','El Cartel de los Soles','555555555','Jackie Chan','Computacion',2007),
-	  ('343275459','El Cartel de los Soles','555555555','Astronema Hernandez','Computacion',2006);
+VALUES('243275454','Los Rompe Codigos','123456789','Tchaikovsky Perez','Computacion',2),
+	  ('568921454','Los Rompe Codigos','123456789','Stravinsky Sanchez','Computacion',2),
+	  ('879554574','Los Rompe Codigos','123456789','Anna Mozart','Computacion',3),
+	  ('243333454','La Faccion EseCuEle','987654321','Donald Chamberlin','Computacion',1),
+	  ('243275455','La Faccion EseCuEle','987654321','Edgar Frank Codd','Computacion',2),
+	  ('243275584','La Faccion EseCuEle','987654321','Helena Tesla','Computacion',3),
+	  ('243587454','La Legion Vtelca','456789357','Bill Gates','Computacion',5),
+	  ('343275454','La Legion Vtelca','456789357','Emma Watson','Computacion',4),
+	  ('563225454','La Legion Vtelca','456789357','Zoraya Montenegro','Computacion',2),
+	  ('25475454','Escuadron Suicida','321987654','Mister Sayaman','Computacion',3),
+	  ('698275454','Escuadron Suicida','321987654','Krilin Feinaiquel','Computacion',1),
+	  ('221457545','Escuadron Suicida','321987654','Son Goku','Computacion',1),
+	  ('235478965','El Cartel de los Soles','555555555','Jhonny Bravo','Computacion',3),
+	  ('227895465','El Cartel de los Soles','555555555','Jackie Chan','Computacion',3),
+	  ('343275459','El Cartel de los Soles','555555555','Astronema Hernande','Computacion',4);
+INSERT INTO mtn.Equipo
+VALUES('Los Rompe Codigos','Universidad de Carabobo','04-03-2008','El Programatonic 2000','Becas de Sueldo Minimo','Excelente'),
+	  ('La Faccion EseCuEle','Universidad de Carabobo','04-03-2008','El Programatonic 2000','Clases con Bill Gates por Messenger Plus','Buenisimo'),
+	  ('La Legion Vtelca','Universidad de Carabobo','09-05-2009','El Maraton Intergalactico de Programacion 23','Clases en el Espacio con Yoda','De otro mundo'),
+	  ('Escuadron Suicida','Universidad de Carabobo','09-05-2009','El Maraton Intergalactico de Programacion 23','Clases en el Espacio con Yoda','De otra Galaxia'),
+	  ('El Cartel de los Soles','Universidad de Carabobo','19-08-2003','Maraton de Programacion Shaolin','Clases de C con Miyagi','Aceptable');
 
 
